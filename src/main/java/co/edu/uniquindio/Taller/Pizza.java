81e76b391a4371281137cc6284bef63826f89eee
eliza-maker <elcuve08@gmail.com>
2026-03-04 00:06:43 -0500

    ejercicio2

--- src/main/java/co/edu/uniquindio/Taller/Pizza.java
+++ src/main/java/co/edu/uniquindio/Taller/Pizza.java
@@ -1,5 +1,22 @@
 package co.edu.uniquindio.Taller;
 
 public class Pizza implements IngredientesFactory {
+    public Pizza() {
+        super();
+    }
 
+    @Override
+    public Salsa CrearSalsa() {
+        return null;
+    }
+
+    @Override
+    public Queso CrearQueso() {
+        return null;
+    }
+
+    @Override
+    public Masa CrearMasa() {
+        return null;
+    }
 }
